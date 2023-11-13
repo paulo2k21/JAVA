9d69e99a2553ab0a3e59a7d54ae7b2dc6f56fb51
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-11-13 11:34:50 -0300

    Iniciando RedeSocial

---
+++ RedeSocial/src/cliente/Usuario.java
@@ -0,0 +1,38 @@
+package cliente;
+
+import java.util.ArrayList;
+
+public class Usuario {
+	
+	private String nome, email, nacionalidade;
+	private String ListaDeEmail;
+	private ArrayList<String> postagens;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	
+
+	
+
+}
